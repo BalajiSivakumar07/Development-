916abcc990b4c6c8001cc438691f4338bc4f14ee
BalajiSivakumar07 <balajisivas06@gmail.com>
2022-01-03 12:02:33 +0530

    todays update

---
+++ Trainer.java
@@ -0,0 +1,31 @@
+public class Trainer
+{
+	String dept  = "java";
+	String institute ="payilagam";
+
+	private int salary= 10000;
+	
+	public Trainer()
+	{
+	
+	}
+	public Trainer (String s, String s1)
+	{
+	
+	}
+	
+	public int getsalary()
+	{
+	 return this.salary;
+	}
+
+	public void training()
+	{
+	System.out.println("Trainer");
+	}
+
+	public static void main(String [] args)
+	{
+	Trainer trainerKumar = new Trainer("CSE", "payilagam");
+	}
+}
